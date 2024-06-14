e5b96d35de1308183b50fec1a3d0f8bb57196100
Marcos Henrique <marcos.henrique@engegraph.com.br>
2024-06-14 00:57:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using MyFirstMLApp;
+
+namespace MyFirstMLApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleData = new SentimentModel.ModelInput() { Col0 = "This restaurant was wonderful." };
+            var result = SentimentModel.Predict(sampleData);
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
